d7123aab7af482e4d54f66bfe2d707edac30c55a
朝永健太 <kentatomonaga@mbp.local>
2021-02-17 21:53:26 +0900

    追加

---
+++ practice/210217/test1.php
@@ -0,0 +1,16 @@
+<?php
+// 再起関数
+$stdin = trim(fgets(STDIN));  
+$array1 = str_split($stdin);
+
+$args = 0;
+abc($args);
+function abc($args) {
+    if ($args == 10) {
+        var_dump($args);
+        return $args;	
+    }
+    return abc($args +1); //abc 関数内で abc 関数を呼び出している
+}
+
+?>
