6b8e7bd61539be7a4762dd078d7e0dd8057efe79
朝永健太 <kentatomonaga@mbp.local>
2021-02-19 00:21:59 +0900

    追加

---
+++ practice/210217/test5.php
@@ -0,0 +1,16 @@
+<?php
+// https://atcoder.jp/contests/abs/tasks/abc083_b
+fscanf(STDIN,'%d%d%d',$n,$a,$b);
+
+$cnt = 0;
+
+for ($i=1; $i<=$n; $i++) {
+    $q_1 = array_sum(str_split($i));
+    // var_dump($q_1);
+    if ($q_1 >= $a and $q_1 <= $b) {
+        $cnt += $i;
+    }
+}
+
+echo $cnt;
+?>
