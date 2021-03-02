2263f1beb104e0a0cc97774dc95d623c5ec39c4c
朝永健太 <kentatomonaga@mbp.local>
2021-03-02 23:10:55 +0900

    add

--- practice/210226/210226_test2.php
+++ practice/210226/210226_test2.php
@@ -1,13 +1,13 @@
 <!-- https://atcoder.jp/contests/abs/tasks/abc086_a -->
 
 <?php
-# 整数の入力
 $array1 = explode(" ", trim(fgets(STDIN)));
 $asnwer1 = $array1[0] * $array1[1];
 if (0 == $asnwer1 % 2) {
+	// 偶数
 	echo "Even";
 } else {
+	// 奇数
 	echo "Odd";
 }
-
 ?>
